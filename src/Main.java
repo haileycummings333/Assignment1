ca4da3342ea6b1ed26ef25cb5d30e97dccc09ff9
haileycummings333 <haileycummings333@gmail.com>
2024-01-25 16:24:54 -0600

    finished commonSubsequence method, need to fix. not given expected output

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        String text1 = "abcde";
-        String text2 = "ace";
-        //expected output would be "a" or "c"
+        String text1 = "abc";
+        String text2 = "def";
+        //expected output would be "ace"
+
+        //print the input
+        System.out.println("Input: ");
+        System.out.println("Text1: " + text1);
+        System.out.println("Text2: " + text2);
 
         //call longestCommonSubsequence method on the two strings
+        String result = longestCommonSubsequence(text1, text2);
 
+        //print the output string and the length
+        System.out.println("");
+        System.out.println("Output: " + result);
+        System.out.println("Length of Longest Common Subsequence: " + result.length());
 
     }
 
-    public static int longestCommonSubsequence(String text1, String text2) {
+    public static String longestCommonSubsequence(String text1, String text2) {
         //store strings as char arrays to iterate through
         char[] str1 = text1.toCharArray();
         char[] str2 = text2.toCharArray();
@@ -18,14 +28,33 @@ public class Main {
         int m = str1.length;
         int n = str2.length;
 
-        //array to store intermediate values
-        int[] intermed = new int[n + 1];
 
+        String commonSubsequence = "";
 
-        //for loops to iterate through the char arrays and
+        int i = 0;
+        int j = 0;
+        //for loop to iterate through the char arrays and
         //compare for matching letters
 
-        return intermed[n];
+        //for each char in the str1 array, compare
+        for (char currentChar : str1) {
+            //no match & within bounds of the string
+            while (i < n && str1[i] != currentChar) {
+                //go to next index
+                i++;
+            }
+
+            // i<n makes sure not to go out of bounds
+            if (i < n) {
+                //add the currentChar to the commonSubseqeunce
+                commonSubsequence += currentChar;
+                //go to next index
+                i++;
+            }
+        }
+        //return the common subsequence, it there is none it will return empty string
+        return commonSubsequence;
+
     }
 
 }
