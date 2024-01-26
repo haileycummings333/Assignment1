b8841f0d4a4011c2098ddb8a8b1765838fc908e9
haileycummings333 <haileycummings333@gmail.com>
2024-01-25 20:06:07 -0600

    start of remove element

---
+++ src/RemoveElement.java
@@ -0,0 +1,14 @@
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        //some values to test with
+
+        int[] nums = {3, 2, 2, 3, 4, 5, 3, 6};
+        int val = 3;
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
+    }
+
+}
